56376cdec7d3540d90b001265bbcb8778fc9bf23
Adam <stula1@gmail.com>
2014-01-24 02:38:07 +0000

    fix indent spacing

--- src/python/check_status.py
+++ src/python/check_status.py
@@ -4,11 +4,11 @@ import time
 import sys
 
 def checkstatus(url):
-	conn = httplib.HTTPConnection("127.0.0.1")
-	conn.request("GET","/status.php?url="+url)
-	resp = conn.getresponse()
-	value = resp.read()
-	return value.lower()
+  conn = httplib.HTTPConnection("127.0.0.1")
+  conn.request("GET","/status.php?url="+url)
+  resp = conn.getresponse()
+  value = resp.read()
+  return value.lower()
 
 closed = "closed"
 open = "open"
@@ -17,14 +17,14 @@ delay = "delay"
 url = sys.argv[1]
 print "Using URL: "+url
 while True:
-	value = checkstatus(url)
-	if value == closed:
-	  print "Schools are closed"
-	elif value == open:
-	  print "Schools are open"
-	elif value == delay:
-	  print "Schools are delayed"
-	else:
-	  print "Unknown status"
+  value = checkstatus(url)
+  if value == closed:
+    print "Schools are closed"
+  elif value == open:
+    print "Schools are open"
+  elif value == delay:
+    print "Schools are delayed"
+  else:
+    print "Unknown status"
 
-	time.sleep(5)
+  time.sleep(5)
